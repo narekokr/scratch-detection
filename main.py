9579779d88fe6ab26dac5d15d4a0959c2672b0ab
Narek <narek.okroyan@fluxtechs.com>
2023-03-27 15:05:41 +0400

    added early stopping

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import argparse
 import datetime
 import time
 
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -9,6 +10,7 @@ from torch.utils.data import DataLoader
 from dataset import ScratchDataset
 from loss import DiceLoss
 from model import UNet
+from util import EarlyStopper
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--batch_size', type=int, default=4)
@@ -31,11 +33,13 @@ model.to(device)
 optimizer = optim.AdamW(model.parameters(), lr=0.01, weight_decay=0.01)
 scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.1)
 criterion = nn.BCEWithLogitsLoss()
+early_stopper = EarlyStopper(patience=3, min_delta=0.001)
 
 # Define the dataset and dataloader
 dataset = ScratchDataset(damaged_dir='train_data/imgs', mask_dir='train_data/masks', image_size=(256, 256))
 train_set, val_set = torch.utils.data.random_split(dataset, [0.8, 0.2])
-dataloader = DataLoader(train_set, batch_size=batch_size, shuffle=True)
+train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True)
+val_loader = DataLoader(val_set, batch_size=batch_size, shuffle=True)
 
 start_epoch = 0
 
@@ -51,7 +55,7 @@ if checkpoint:
 start = time.time()
 
 for epoch in range(start_epoch, epochs):
-    for i, data in enumerate(dataloader):
+    for i, data in enumerate(train_loader):
         inputs, masks = data
         inputs, masks = inputs.to(device), masks.to(device)
 
@@ -64,7 +68,25 @@ for epoch in range(start_epoch, epochs):
 
         taken = datetime.timedelta(seconds=(time.time() - start))
         formatted_time = str(taken).split('.')[0]
-        print('[%d, %5d] loss: %.3f, time taken:' % (epoch + 1, i + 1, loss.item()), formatted_time)
+        print('train [%d, %5d] loss: %.3f, time taken:' % (epoch + 1, i + 1, loss.item()), formatted_time)
+
+    with torch.no_grad():
+        losses = []
+        for i, data in enumerate(val_loader):
+            inputs, masks = data
+            inputs, masks = inputs.to(device), masks.to(device)
+
+            outputs = model(inputs)
+            loss = criterion(outputs, masks)
+            losses.append(loss.item())
+
+            taken = datetime.timedelta(seconds=(time.time() - start))
+            formatted_time = str(taken).split('.')[0]
+            print('validate [%d, %5d] loss: %.3f, time taken:' % (epoch + 1, i + 1, loss.item()), formatted_time)
+        if early_stopper.early_stop(np.mean(losses)):
+            break
+
+    scheduler.step()
 
     if epoch % 10 == 0:
         torch.save({
